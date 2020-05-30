abe39f16e080490dec975181a13551663a3bb447
Aditya Prakash <201351010@iiitvadodara.ac.in>
2020-05-30 18:25:18 -0500

    Meeting Rooms 2

---
+++ M235.java
@@ -0,0 +1,195 @@
+apdec19@gmail.com
+  Qwertyuiop1
+Spectrum id after disconnect: 1001-3235-7951-1042
+Flight travel credit of $151.98 for Southwest airlines on confirmation number MAPM2B expiry 09/07/2022
+recruiter message direct simran and priyanka, handshake
+
+I am currently working as a Software Engineer for Axxess.
+I have previously worked as a Software Developer Intern for Unity Technologies and a Unity Developer at UT Dallas.
+I have worked with Unity and VR based applications. I have worked with MyndVR on an Android and VR based application. I developed a 360 Video player in Unity.
+I am currently working on an AR-based application to detect a 2D object and rotate and translate a Virtual 3D object in Unity.
+I have completed my Masters(Computer Science) from The University of Texas at Dallas. I have interned at Atos where my task was C++ OpenCV programming.  
+I also love working on the UI/UX part of the projects. I am familiar with C#, .NET, SQL and Visual Studio to create Winforms/.NET applications. 
+I am currently looking for a full-time job starting in October 2019.
+Linkedin : https://www.linkedin.com/in/adi-prakash/
+GitHub : https://github.com/adityaiiitv
+Portfolio : https://connect.unity.com/u/5c69e2d8edbc2a001fd115b3
+
+ashok mago
+geoffrey dagley -  AR class guest, linkedin messaged, now start conversation
+oleg vishnepolsky
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+General: 
+
+Why me?
+  Confident with my skills and despite the requirements, I am hungry to be successful
+  Fast learner
+  Competitive
+  Problem solving aptitude
+  Passion for work
+  persistent
+  futuristic, i keep expanding my limits
+
+Rate yourself question
+  9/10 as I am still learning this technique
+  rated by my seniors so I feel 9, but there is always room for improvement
+
+Overqualification?
+  if you want a doctor/pilot, qualified or overqualified one?
+
+Weakness?
+  sometimes i am impatient, anxious before a big event or before a conversation
+  sometimes i waste time at that time when i am anxious instead of putting it to good use
+  but i am overcoming it with meditation
+  
+What would your friends describe u as?
+  My best friend who knows me from 10 years says that I am persistent
+  My good college friend says I am trustworthy
+  My good friend at UTD says I am fun to be around and studious
+  My high school friend says I am willing to go extra mile for the cause i believe in
+  My cricket team members say I am a good leader on the pitch, that I understand what is going in the game
+
+what motivates you?
+  competition, chance to grow and be better than I was yesterday, people like cristiano ronaldo
+
+how do you handle rejection?
+  Nobody loves rejection, but if you want to succeed, must not take it personally, learn from it and apply it in your next approach
+  sometimes i get an idea which seems amazing but is rejected, i learn what i did wrong and how can i improve my suggestions
+
+How could you help the company remain organized and efficient
+  It starts with a node, if i am organized, i can help, i document my daily work, i document my work at UTDallas, checklist, info
+
+Unity:
+
+struct vs class unity - pass by memory or pass by reference
+coroutines unity
+java memory leak
+Texture and Mesh are not thread proof
+Vertex shader - script that runs for each matrix, allows dev to allow transformation
+Pixel shader - runs for each pixel after 3 vertices have been processed
+Deferred lighting - If scene is large, if pixels are provided light then huge over head, in deferred lighting draw without light,
+                    then give light to the pixels visible in scene, so faster
+Visualization mode - gives you number of pixels rendered in an area, yellow to white are hot
+Time.DeltaTime - smooth transition as FPS may vary from 40 to 60 FPS
+Vector normalization before movement - so that there is uniform steps
+For trigger, one of the colliding objects must have rigid body attached to it
+Direct access to methods is faster than indirect methods
+
+SQL
+  1NF - atomic values
+  2NF - no partial dependency on PK
+  3NF - no transitive dependency
+  BCNF - for each X->Y, X should be a super key
+  4NF - no multi valued dependency
+  CREATE VIEW view_name AS SELECT ..........
+  CREATE INDEX index_name ON table_name (column1, column2, ...);   helps speeding up select and where but slows down data input
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+American Haval Motor technology
+May 11th call this guy, for a job:
+248-880-3146
+shreyas.nagaraj@havalus.com
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+Scientific games
+https://www.geeksforgeeks.org/program-bridge-torch-problem/
+
+Why do you want to work for us?
+    
+why do you want to be a game designer?
+Experience with math based games - love bluff, tried poker, love card games, 3D calculation based games
+
+2010
+  Char ch='B'
+  If you want it to be const
+  Then it should be
+  Const char cch="A"
+
+2015
+  Binary search
+  Tell me about a situation where you've had to juggle work loads
+    Doing masters is same, but on STEPP also
+
+2016
+  How will you Lock an object so that only 1 thread can access it in java? 
+    by synchronized keyword, snyced on an object
+  1. What is polymorphism? What is static and dynamic polymorphism
+    static - method overloading as done at compile tiem
+    dynamic - method overriding, using virtual funtion, done at run time
+  2. What is virtual functions?
+    function in base class overrided in derived class
+  3. What is vtable and vptr?
+    vptr - hidden pointer in c++, when there are more than 1 virtual functions
+    vtable - virtual function table, each row is a function pointer of a corresponding virtual function
+  4. What are the type of design pattern? Explain it. Tell about subtype of that.
+    general reusable solution to commonly occuring problem
+  5. Which design pattern you have used in you project? How will you decide which design pattern to use where?
+    never used
+  6. What is singleton design
+    only one instance of class
+    https://www.geeksforgeeks.org/singleton-design-pattern/
+  7. Write small fun for operator overloading for + operator
+    https://www.tutorialspoint.com/cplusplus/cpp_overloading.htm
+  8. Why we use const in in prototype of operator overloading?
+    it passes a pointer, and it does not want the parameter to be changed by the function
+  9. class test {
+        int funct( void ) const;
+         };
+  what it will return?
+  ->There is a pointer to the object for which a function is called known as the 'this' pointer. By default the type of 'this' is X *const ( a constant pointer). The const keyword changes the type to const X *const ( a constant pointer to constant data ).
+  10. Difference betn vector and list? How will u decide which one to use
+    List is fast as it is not synchornized, vector is slow
+    List uses Iterator, Vector uses iterator or enumerable
+    List is not legacy(old) class, vector is
+  11. You have a class called Math which having 2 virtual fun add(), sub(). How many vtable will be created for this class and how it looks like??
+  sql-> joins, 
+  delete duplicate entries, 
+    https://stackoverflow.com/questions/18390574/how-to-delete-duplicate-rows-in-sql-server
+  salary on nth position ,
+    https://stackoverflow.com/questions/16234983/how-to-find-third-or-nth-maximum-salary-from-salary-table
+  c# ->about collection,refrence type and value type variables,many more,mvc
+  Polymorphism vs Inheritance 
+
+2018
+  1.Multi threading concepts
+  2.Design patterns(mostly singleton)
+  3.Memory management
+  4.Recursion
+  5.Delegates
+    https://www.tutorialsteacher.com/csharp/csharp-delegates
+  max population algorithm.
+  are you aware of agile software development cycle?  
+
+2019
+  anagram
+  Find even or odd without conditional statements
+    https://www.geeksforgeeks.org/even-or-odd-without-using-condtional-statement/
+  Square numbers without multiplication or any inbuilt functions
+  Delegates event,
+  bubble sort with recursion,
+    https://www.geeksforgeeks.org/recursive-bubble-sort/ 
+ TPL and many scenario based problems to solve in C# 
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+Unity
+
+Joseph Scheinberg
+Paul Burslem
+Victor Willyams
+Stephen Palmer
+
+what makes a good team?
+  Good communication to avoid conflicts and improve progress
+  They focus on goals and results
+  Everyone contributes a fair share
+  They offer support to each other
+  Diverse skills of team members
+  Good leader
+  Organized
+  They enjoy what they are doing
+
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------
